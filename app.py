4c716a1e83d974cd841b4402ccf1b41c9cddaadd
IRFAN ULLAH KHAN <134633289+programmarself@users.noreply.github.com>
2024-09-06 21:06:16 +0500

    Update app.py

--- app.py
+++ app.py
@@ -188,7 +188,7 @@ st.markdown("""
     </section>
 """, unsafe_allow_html=True)
 # Sections
-st.image("doctor ai.png")
+st.image("doctor.png")
 st.markdown("<div id='home'></div>", unsafe_allow_html=True)
 st.header("Welcome to HealthCare Center")
 st.write("At Doctor AI, we leverage cutting-edge technology to deliver exceptional healthcare services tailored to your needs. Our team of experts is here to support your health journey.")
